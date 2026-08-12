d91291e2b7f32dfd993e0423e587746ffff32cc5
naren <narenmagarz98@gmail.com>
2026-08-12 06:31:46 +0545

    implement proxy server

---
+++ backend-server/server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+function buildServer(port) {
+	const server = http.createServer((req, res) => {
+        console.log(req.socket.remotePort, 'port')
+		if (req.url === "/api/health") {
+			return res.end("Okay");
+		}
+
+		res.setHeader("content-type", "application/json");
+		res.statusCode = 200;
+		return res.end(JSON.stringify({ status: "Okay", message: "successful" }));
+	});
+	server.listen(port, () => {
+		console.log("server is running on port", port);
+	});
+}
+
+buildServer(process.env.PORT || 6001);
--- proxy-server/proxy.js
+++ proxy-server/proxy.js
@@ -1,29 +1,84 @@
-/*
+const http = require("http");
+const murmur = require("murmurhash3js");
+const { exec } = require("node:child_process");
 
-    request -> proxy server -> multiple instances of server
+const backendServers = new Map();
 
-*/
+function buildProxy(port) {
+	http
+		.createServer(async (req, res) => {
+			const targetServer = getServer(req.socket.remotePort);
+			const targetUrl = new URL(targetServer);
+            console.log(targetUrl, 'target url')
+			const proxyReq = http.request(
+				{
+					hostname: targetUrl.hostname,
+					host: targetUrl.host,
+					path: targetUrl.url,
+					headers: targetUrl.headers,
+					method: targetUrl.method,
+				},
+				(proxyRes) => {
+					res.writeHead(proxyRes.statusCode ?? 500, proxyRes.headers);
+					proxyRes.pipe(res);
+				},
+			);
 
-const http = require('http');
+			proxyReq.on("error", (err) => {
+				console.error(err);
 
-const backendServers = [
-    "http://localhost:8000",
-    "http://localhost:8001",
-    "http://localhost:8002",
-]
+				if (!res.headersSent) {
+					res.writeHead(502);
+				}
 
-http.createServer(async (req, res) => {
-    await distributeRequests(req);
-})
+				res.end("Bad Gateway");
+			});
 
-async function distributeRequests(req) {
-    const server = backendServers.length % ( backendServers.length - 1 )
-
-    const baseUrl = `http://localhost:8001/${req.url}`
-    const response = await fetch(baseUrl, {
-        method: req.method,
-        headers: req.headers
-    });
-
-    return response;
+			req.pipe(proxyReq);
+		})
+		.listen(port);
 }
+
+function hash(key) {
+	return murmur.x86.hash32(key);
+}
+
+function getServer(key) {
+    const hashed = hash(key);
+    console.log(hashed)
+	const index = hashed % backendServers.length;
+    console.log(index, 'index')
+	return backendServers[index];
+}
+
+function discoverServers() {
+	setInterval(() => {
+		exec("lsof -nP -iTCP -sTCP:LISTEN", async (error, stdout) => {
+			if (error) {
+				console.error(error);
+				return;
+			}
+
+            const process = stdout.split("\n");
+			const regex = /^node.*\*:(6\d+)/;
+			const serverPorts = process.filter((process) => process.match(regex)).map((process) => Number(process.match(regex)?.[1]));
+
+			// will discover healthy servers
+			for (const port of serverPorts) {
+				const url = `http://localhost:${port}/api/health`;
+				try {
+					const response = await fetch(url);
+					if (response.ok) {
+						console.log(`${port} is healthy`);
+						backendServers.set(port, url);
+					}
+				} catch (error) {
+					console.log(`${port} is unreachable`);
+				}
+			}
+		});
+	}, 2_000);
+}
+
+discoverServers();
+buildProxy(process.env.PORT ?? 5001);
